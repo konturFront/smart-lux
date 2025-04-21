d215ba2dbb0065a486b8a3751bcf273a16de99ce
Сергей Блохин <sblohin@cloudfactory.ru>
2025-04-21 11:39:50 +0300

    подготовка

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,17 +4,26 @@ import { Header } from '../Header/Header';
 import { Wrapper } from '../Wrapper/Wrapper';
 import { state } from '../../store/store';
 import styles from './styles.module.scss';
+import { useDeviceDetect } from '../../hooks/useDeviceDetect';
 export const Layout = ({ children }: { children?: preact.ComponentChildren }) => {
+  const { isMobile } = useDeviceDetect();
   useEffect(() => {
     socketService.connect(state.value.socketURL);
     return () => {
       socketService.disconnect();
     };
   }, []);
-  return (
-    <div>
-      <Header />
-      <Wrapper>{children}</Wrapper>
-    </div>
-  );
+
+  if (isMobile) {
+    return (
+      <div>
+        <Header />
+        <Wrapper>{children}</Wrapper>
+      </div>
+    );
+  }
+
+  if (!isMobile) {
+    return <div style={{ fontSize: '40px' }}>Only Mobile Version</div>;
+  }
 };
